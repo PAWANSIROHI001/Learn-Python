29ff30ed9d003cf34a1a95ed378da56e79d8a9ab
PAWAN SIROHI <MONTEY786SIROHI@GMAIL.COM>
2026-06-02 17:38:55 +0530

    Learn Python

---
+++ day2.py
@@ -0,0 +1,24 @@
+# In second day we are using the escape sequence characters
+# common escape sequence characters are: # \n, \t, \\, \', \", etc.
+
+print("hello world")
+
+print("hello\nworld") # \n is used to print in new line
+print("hello\tworld") # \t is used to print in tab space
+print("hello\\world") # \\ is used to print \ in the output
+print("hello\'world") # \' is used to print ' in the output
+print("hello\"world") # \" is used to print " in the output
+
+# this is all single line comment.
+
+#  we are also using multi line comment in python which is done by using triple quotes (''' or """).
+# such as:
+'''
+Ramu is a good boy.
+He is very intelligent.
+'''
+
+"""
+Ramu is a good boy
+He is very intelligent.
+"""
