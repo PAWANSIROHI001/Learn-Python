9d1b8e9fb92b48e67b9fb46160d0679240d80bca
PAWAN SIROHI <MONTEY786SIROHI@GMAIL.COM>
2026-06-03 15:37:46 +0530

    Learn Python

---
+++ day3.py
@@ -0,0 +1,27 @@
+# variables
+# variable like a container that holds data
+# variable can be of different types like string, integer, float, boolean, etc.
+
+# e.g:
+
+a= 2026
+print(a)
+
+b= "sirohi"
+print(b)
+
+c= 3.14
+print(c)    
+
+d= True
+print(d)
+
+# we can also find the type of variable by using type() function
+print("type of a is", type(a)) # <class 'int'>
+print("type of b is", type(b)) # <class 'str'>
+print("type of c is", type(c)) # <class 'float'>
+print("type of d is", type(d)) # <class 'bool'>
+
+# thats all for variables in python. In next day we will learn about operators in python.
+
+
