d7b291caa3e9412cb66e0c135d8d4b685da8858d
PAWAN SIROHI <MONTEY786SIROHI@GMAIL.COM>
2026-06-05 18:06:13 +0530

    Learn Python

---
+++ day7.py
@@ -0,0 +1,23 @@
+# using is string 
+
+# using the double quotes to define a string
+
+a = "hello world"   
+print(a)    
+
+# using the single quotes to define a strin
+
+b = 'hello world'
+print(b)
+
+# using the triple quotes to define a string
+
+c = """hello world"""
+print(c)
+
+# using the triple quotes to define a string with multiple lines
+
+d = """hello sirohi how are you """
+print(d)
+
+
