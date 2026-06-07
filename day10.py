71883414bdfb54f76019d9e6f5d8af4dd41e33af
PAWAN SIROHI <MONTEY786SIROHI@GMAIL.COM>
2026-06-07 22:14:02 +0530

    Learn Python

---
+++ day10.py
@@ -0,0 +1,23 @@
+# loops in python
+
+# first is for loop
+
+for i in range(10):
+    print(i)
+
+
+# example
+
+name = "sirohi"
+
+for i in name:
+    print(i)
+
+
+# we can also use for loop with list
+
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)    #and we can also use if statement in for loop
+    for i in fruit:
+     print(i)
