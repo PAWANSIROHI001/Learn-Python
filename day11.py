8324f058619033998a478084b136dbdbaff1f77e
PAWAN SIROHI <MONTEY786SIROHI@GMAIL.COM>
2026-06-07 22:35:33 +0530

    Learn Python

---
+++ day11.py
@@ -0,0 +1,29 @@
+# Loops in Python
+
+# second is while loop
+
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+
+# Another example
+
+
+count = 9
+while (count > 0):
+   print(count)
+   count = count - 1
+else:
+    print("count is zero")
+
+
+
+# one more example
+
+i = int(input("Enter a number: "))
+while i > 0:
+    print(i)
+    i -= 1
+              
+
