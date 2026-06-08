2190d74c86e6a6d0e8bd9a35d59eb134600ecd5e
PAWAN SIROHI <MONTEY786SIROHI@GMAIL.COM>
2026-06-08 17:33:59 +0530

    Learn Python

---
+++ day12.py
@@ -0,0 +1,24 @@
+# break and continious statement
+
+# Break Statement remove from the loop
+
+# example:
+
+for i in range(12):
+    if(i == 10):
+        break
+    print("5x", i+1, "=", 5* (i+1))
+
+
+
+# continue statement
+
+# continue statement skip the iteration.
+
+for i in range(12):
+    if(i == 10):
+        print("skip the iteration")
+        continue
+    print("5x", i+1, "=", 5* (i+1))
+
+
